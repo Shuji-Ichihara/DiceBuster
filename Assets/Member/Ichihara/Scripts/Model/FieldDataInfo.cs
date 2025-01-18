6f7a11b078d70726652c477851e23df34fa8607c
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2025-01-18 17:39:53 +0900

    Create FieldDataInfo
    jsonファイルからマスのデータを読み込むクラスを作成した。

---
+++ Assets/Member/Ichihara/Scripts/Model/FieldDataInfo.cs
@@ -0,0 +1,51 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FieldType
+{
+    Normal,
+    Attack,
+    Health,
+    Guard,
+}
+
+[System.Serializable]
+public class FieldData
+{
+    public FieldType FieldType;
+    public int SizeOfEffect;
+}
+
+public class FieldDataInfo : MonoBehaviour
+{
+    // マスの設定ファイル
+    [SerializeField]
+    private TextAsset _fieldInfoJson = null;
+    // マスの情報のデータリスト
+    [System.NonSerialized]
+    public List<FieldData> FieldDataList = new List<FieldData>();
+
+    /// <summary>
+    /// Jsonデータから効果マスの情報を読み込む
+    /// </summary>
+    /// <returns></returns>
+    public async UniTask LoadFieldData()
+    {
+        FieldData fieldData = new FieldData();
+        string fieldInfoJson = _fieldInfoJson.ToString();
+        JsonNode json = JsonNode.Parse(fieldInfoJson);
+        foreach (JsonNode node in json["FieldData"])
+        {
+            // マスの効果の読み込み
+            fieldData.FieldType = (FieldType)Enum.Parse(typeof(FieldType), node["FieldType"].Get<string>());
+            // 効果量の読み込み
+            fieldData.SizeOfEffect = int.Parse(node["SizeOfEffect"].Get<string>());
+            // マスのデータをリストに追加
+            FieldDataList.Add(fieldData);
+            await UniTask.Yield();
+        }
+    }
+}
