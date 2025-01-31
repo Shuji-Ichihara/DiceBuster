d89711a4e9f354cf2c8b3c419ad3fe59e1db393d
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2025-02-01 00:17:53 +0900

    Update EnemyGenerater.cs
    エネミーを管理する変数を配列に変更した。

--- Assets/Member/Ichihara/Scripts/View/EnemyGenerater.cs
+++ Assets/Member/Ichihara/Scripts/View/EnemyGenerater.cs
@@ -7,17 +7,16 @@ public class EnemyGenerater : SingletonMonoBehaviour<EnemyGenerater>
     [SerializeField]
     private List<Enemy> _enemies = new List<Enemy>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void GenerateEnemy()
     {
-        Instantiate(_enemies[0], FieldGenerater.Instance.GetGridPosition(0,2), Quaternion.identity);
-        Instantiate(_enemies[1], FieldGenerater.Instance.GetGridPosition(2,6), Quaternion.identity);
-        Instantiate(_enemies[2], FieldGenerater.Instance.GetGridPosition(4,8), Quaternion.identity);
+        _enemies[0] = Instantiate(_enemies[0], FieldGenerater.Instance.GetGridPosition(0, 2), Quaternion.identity);
+        _enemies[1] = Instantiate(_enemies[1], FieldGenerater.Instance.GetGridPosition(2, 6), Quaternion.identity);
+        _enemies[2] = Instantiate(_enemies[2], FieldGenerater.Instance.GetGridPosition(4, 8), Quaternion.identity);
+    }
+
+    public List<Enemy> GetEnemyList()
+    {
+        return _enemies;
     }
 
 }
